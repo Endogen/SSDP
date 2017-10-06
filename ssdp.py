b10a7e7c42ed9da1d53d815331780a4d0427a9ea
David Strohmayer <Endogen@users.noreply.github.com>
2017-10-06 10:05:03 +0200

    * Changed back to the original script
    Python 2 version

--- ssdp.py
+++ ssdp.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
-
-# Source: https://blog.cloudflare.com/ssdp-100gbps/
-
-import socket
+#!/usr/bin/env python2
+import socket  
 import sys
 
-dst = "239.255.255.250"
-if len(sys.argv) > 1:
+dst = "239.255.255.250"  
+if len(sys.argv) > 1:  
     dst = sys.argv[1]
-st = "upnp:rootdevice"
-if len(sys.argv) > 2:
+st = "upnp:rootdevice"  
+if len(sys.argv) > 2:  
     st = sys.argv[2]
 
-msg = [
+msg = [  
     'M-SEARCH * HTTP/1.1',
     'Host:239.255.255.250:1900',
     'ST:%s' % (st,),
@@ -20,13 +17,13 @@ msg = [
     'MX:1',
     '']
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-s.settimeout(10)
-s.sendto(str.encode('\r\n'.join(msg)), (dst, 1900))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  
+s.settimeout(10)  
+s.sendto('\r\n'.join(msg), (dst, 1900) )
 
-while True:
+while True:  
     try:
         data, addr = s.recvfrom(32*1024)
     except socket.timeout:
         break
-    print("[+] %s\n%s" % (addr, data))
+    print "[+] %s\n%s" % (addr, data)
