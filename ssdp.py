67fa2a737918bf00bfab4e8000b0d97183148e77
Endogen <081081@gmx.net>
2019-07-30 22:59:22 +0200

    Converted to Python 3

--- ssdp.py
+++ ssdp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Source: https://blog.cloudflare.com/ssdp-100gbps/
+# Source: https://blog.cloudflare.com/ssdp-100gbps
 
 import socket
 import sys
@@ -8,17 +8,18 @@ import sys
 dst = "239.255.255.250"
 if len(sys.argv) > 1:
     dst = sys.argv[1]
+
 st = "upnp:rootdevice"
 if len(sys.argv) > 2:
     st = sys.argv[2]
 
 msg = [
-    'M-SEARCH * HTTP/1.1',
-    'Host:239.255.255.250:1900',
-    'ST:%s' % (st,),
-    'Man:"ssdp:discover"',
-    'MX:1',
-    '']
+    f'M-SEARCH * HTTP/1.1',
+    f'Host:239.255.255.250:1900',
+    f'ST:{st}',
+    f'Man:"ssdp:discover"',
+    f'MX:1',
+    f'']
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 s.settimeout(10)
@@ -29,4 +30,4 @@ while True:
         data, addr = s.recvfrom(32*1024)
     except socket.timeout:
         break
-    print("[+] %s\n%s" % (addr, data))
+    print(f"[+] {addr}\n{bytes.decode(data)}")
